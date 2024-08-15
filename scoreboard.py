cec14b02e89eba280e054489ae3111d9dbbf8524
Soumik Dalei <soumikdalei@gmail.com>
2024-08-15 07:39:31 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -22,9 +22,7 @@ class scoreboard(Turtle):
                 data.write(f"{self.high_score}")
         self.score=0
         self.update()
-    # def game_over(self):
-    #     self.goto(0,0)
-    #     self.write("Game Over",align="center",font=("Arial",24,"normal"))
+    
     def increase_score(self):
         self.score+=1
 
